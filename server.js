f2d59206557d5126d628c99484a2fdb2f3bbcae9
a-sha0234 <asharma_31@hotmail.com>
2022-03-11 16:37:51 +0000

    add views for items and categories

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("index", { title: "home" });
+});
+
+app.get("/items", (req, res) => {
+  res.render("items", { title: "items" });
+});
+
+app.get("/categories", (req, res) => {
+  res.render("items", { title: "categories" });
 });
 
 app.listen(3000);
