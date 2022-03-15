6b70b21da9edcc2c01d1d4371cb7c237335dc15b
a-sha0234 <asharma_31@hotmail.com>
2022-03-15 23:01:37 +0000

    remove functions routing functions

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const { default: mongoose } = require("mongoose");
 const monoogse = require("mongoose");
 const food = require("./models/foodItem");
-
+const itemRouters = require("./routes/itemRoutes");
+const categoryRouter = require("./routes/categoryRoutes");
 const fooditem = require("./models/foodItem");
 const app = express();
 
@@ -22,64 +23,66 @@ app.get("/", (req, res) => {
   res.render("index", { title: "home" });
 });
 
-app.get("/items", (req, res) => {
-  fooditem
-    .find()
-    .sort({ createAt: -1 })
-    .then((result) => {
-      res.render("items", { title: "items", food: result });
-    });
-});
+// app.get("/items", (req, res) => {
+//   fooditem
+//     .find()
+//     .sort({ createAt: -1 })
+//     .then((result) => {
+//       res.render("items", { title: "items", food: result });
+//     });
+// });
 
-app.get("/categories", (req, res) => {
-  fooditem
-    .find()
+// app.get("/categories", (req, res) => {
+//   fooditem
+//     .find()
 
-    .then((result) => {
-      res.render("categories", { title: "categories", categories: result });
-    });
-});
+//     .then((result) => {
+//       res.render("categories", { title: "categories", categories: result });
+//     });
+// });
 
 app.get("/create", (req, res) => {
   res.render("createItem", { title: "create your item!" });
 });
 
+app.use(itemRouters);
+app.use(categoryRouter);
 //===========================================
 //POST routes
 //===========================================
 
-app.post("/items", (req, res) => {
-  const item = new fooditem(req.body);
+// app.post("/items", (req, res) => {
+//   const item = new fooditem(req.body);
 
-  item
-    .save()
-    .then((result) => {
-      res.redirect("/items");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+//   item
+//     .save()
+//     .then((result) => {
+//       res.redirect("/items");
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
 
 //===========================================
 //url params
 //===========================================
 
-app.get("/items/:id", (req, res) => {
-  const id = req.params.id; // get the id of the url, this also lets us find this unique id in the database
-  // so we can retrive the correct item that was clicked on
-  fooditem.findById(id).then((result) => {
-    res.render("foodDetails", { title: "details", foodDetails: result });
-  });
-});
+// app.get("/items/:id", (req, res) => {
+//   const id = req.params.id; // get the id of the url, this also lets us find this unique id in the database
+//   // so we can retrive the correct item that was clicked on
+//   fooditem.findById(id).then((result) => {
+//     res.render("foodDetails", { title: "details", foodDetails: result });
+//   });
+// });
 
-app.get("/categories/:id", (req, res) => {
-  const id = req.params.id; // get the id of the url, this also lets us find this unique id in the database
-  // so we can retrive the correct item that was clicked on
-  fooditem.findById(id).then((result) => {
-    res.render("foodDetails", { title: "details", foodDetails: result });
-  });
-});
+// app.get("/categories/:id", (req, res) => {
+//   const id = req.params.id; // get the id of the url, this also lets us find this unique id in the database
+//   // so we can retrive the correct item that was clicked on
+//   fooditem.findById(id).then((result) => {
+//     res.render("foodDetails", { title: "details", foodDetails: result });
+//   });
+// });
 
 //===========================================
 //404 page
