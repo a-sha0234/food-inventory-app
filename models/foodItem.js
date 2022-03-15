97027ae5ebd146a85c005f5f94185cbea8249091
a-sha0234 <asharma_31@hotmail.com>
2022-03-15 00:40:11 +0000

    create schema for form data to then be inserted in database

---
+++ models/foodItem.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const foodSchema = new Schema({
+  name: { type: String, required: true },
+  Category: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  foodDescription: { type: String, required: true },
+  price: { type: Number, required: true },
+  stock: { type: Number, required: true },
+});
+
+const food = mongoose.model("fooditem", foodSchema);
+
+module.exports = food;
