4d4d97fee192c5777477e92ef45865d40f37bf20
a-sha0234 <asharma_31@hotmail.com>
2022-03-16 01:55:15 +0000

    delete comments

--- server.js
+++ server.js
@@ -23,66 +23,12 @@ app.get("/", (req, res) => {
   res.render("index", { title: "home" });
 });
 
-// app.get("/items", (req, res) => {
-//   fooditem
-//     .find()
-//     .sort({ createAt: -1 })
-//     .then((result) => {
-//       res.render("items", { title: "items", food: result });
-//     });
-// });
-
-// app.get("/categories", (req, res) => {
-//   fooditem
-//     .find()
-
-//     .then((result) => {
-//       res.render("categories", { title: "categories", categories: result });
-//     });
-// });
-
 app.get("/create", (req, res) => {
   res.render("createItem", { title: "create your item!" });
 });
 
 app.use(itemRouters);
 app.use(categoryRouter);
-//===========================================
-//POST routes
-//===========================================
-
-// app.post("/items", (req, res) => {
-//   const item = new fooditem(req.body);
-
-//   item
-//     .save()
-//     .then((result) => {
-//       res.redirect("/items");
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// });
-
-//===========================================
-//url params
-//===========================================
-
-// app.get("/items/:id", (req, res) => {
-//   const id = req.params.id; // get the id of the url, this also lets us find this unique id in the database
-//   // so we can retrive the correct item that was clicked on
-//   fooditem.findById(id).then((result) => {
-//     res.render("foodDetails", { title: "details", foodDetails: result });
-//   });
-// });
-
-// app.get("/categories/:id", (req, res) => {
-//   const id = req.params.id; // get the id of the url, this also lets us find this unique id in the database
-//   // so we can retrive the correct item that was clicked on
-//   fooditem.findById(id).then((result) => {
-//     res.render("foodDetails", { title: "details", foodDetails: result });
-//   });
-// });
 
 //===========================================
 //404 page
@@ -91,5 +37,3 @@ app.use(categoryRouter);
 app.use((req, res) => {
   res.status(404).render("404", { title: "404" });
 });
-
-//a1 ,test
